494cebed365d44cdd5d06d5473344722be8c7ce8
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-10-05 14:48:00 +0000

    Fix main.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 mod grosfichiers;
 mod domaines_grosfichiers;
 
+use log::{info};
+use millegrilles_common_rust::tokio as tokio;
+use crate::domaines_grosfichiers::run;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    info!("Demarrer le contexte");
+    executer()
+}
+
+#[tokio::main(flavor = "current_thread")]
+// #[tokio::main(flavor = "multi_thread", worker_threads = 5)]
+async fn executer() {
+    run().await
+}
+
+#[cfg(test)]
+pub mod test_setup {
+    use log::{debug};
+
+    pub fn setup(nom: &str) {
+        let _ = env_logger::builder().is_test(true).try_init();
+        debug!("Running {}", nom);
+    }
 }
